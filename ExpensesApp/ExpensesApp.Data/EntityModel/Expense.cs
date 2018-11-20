cd2e510e9b7f11289863e2d27e970f18c85168a7
Bryan Silverio <bryansilverio12@gmail.com>
2018-11-20 11:24:49 -0600

    add ExpensesApp.Data add ExpensesApp.Core add ExpensesApp.API
    add classes into ExpensesApp.Data

---
+++ ExpensesApp/ExpensesApp.Data/EntityModel/Expense.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpensesApp.Data.EntityModel
+{
+    public class Expense
+    {
+        public int Expense_Id { get; set; }
+        public DateTime Date { get; set; }
+        public decimal Mount { get; set; }
+        public int Category_Id { get; set; }
+        public int User_Id { get; set; }
+
+        public Category Category { get; set; }
+        public User User { get; set; }
+    }
+}
---
+++ ExpensesApp/ExpensesApp.Data/EntityModel/Income.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpensesApp.Data.EntityModel
+{
+    public class Income
+    {
+        public int Income_Id { get; set; }
+        public DateTime Date { get; set; }
+        public int User_Id { get; set; }
+
+        public User User { get; set; }
+    }
+}
